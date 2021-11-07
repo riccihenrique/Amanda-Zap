0a46a21754768d09a84c151b14138884e0ae31cf
Henrique Ricci <ricci.henrique@outlook.com>
2021-11-07 11:32:03 -0300

    fix multi device

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const wa = require('@open-wa/wa-automate');
 
 const app = express();
-let client = {};
 
 const pessoas = {
     'duda': 'mor 💙',
@@ -14,31 +13,31 @@ const pessoas = {
     'mateus': 'matias',
 };
 
+const start = (client) => {
+    app.get('/sendmessage', async (req, res) => {
+        const { name, message } = req.query;
+        if(!name) res.status(400).json({ message: 'Nome não reconhecido' });
+        if(!message) res.status(400).json({ message: 'Mensagem não reconhecida' });
+
+        try {
+            const contacts = await client.getAllContacts();
+            const contact = contacts.filter(({ name: contactName }) => contactName && contactName.toLowerCase() === pessoas[name])[0];
+            if(!contact) res.status(400).json({ message: 'Contato não encontrado' });
+            await client.sendText(contact.id, message);
+            res.end();
+        }
+        catch(ex) {
+            console.error(ex.message);
+            res.status(500).end();
+        }
+    });
+
+    app.get('/', (req, res) => res.send('Ok'));
+
+    app.listen(9090, _ => console.log('Rodando na porta 9090'));
+}
+
 wa.create({
-    mutliDevice: true,
+    multiDevice: true,
     sessionId: 'alexa',
-}).then(cli => {
-    client = cli;
-});
-
-app.get('/sendmessage', async (req, res) => {
-    const { name, message } = req.query;
-    if(!name) res.status(400).json({ message: 'Nome não reconhecido' });
-    if(!message) res.status(400).json({ message: 'Mensagem não reconhecida' });
-
-    try {
-        const contacts = await client.getAllContacts();
-        const contact = contacts.filter(({ name: contactName }) => contactName && contactName.toLowerCase() === pessoas[name])[0];
-        if(!contact) res.status(400).json({ message: 'Contato não encontrado' });
-        await client.sendText(contact.id, message);
-        res.end();
-    }
-    catch(ex) {
-        console.error(ex.message);
-        res.status(500).end();
-    }
-});
-
-app.get('/', (req, res) => res.send('Ok'));
-
-app.listen(9090, _ => console.log('Rodando na porta 9090'))
+}).then(start);
